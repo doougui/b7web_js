9a3ae27d65a0d78eddf7666686e814a2b0a12707
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-10-01 16:14:24 -0300

    "getElementById innerHTML" lesson added

---
+++ Aulas/modulo1_introducao/getelementid_innerhtml/script.js
@@ -0,0 +1,12 @@
+// Usando em uma linha só
+document.getElementById("titulo").innerHTML = "Olá mundo!";
+document.getElementById("paragrafo").innerHTML = "Novo conteúdo.";
+
+// Inserindo o elemento em uma variável
+var titulo = document.getElementById("titulo");
+titulo.innerHTML = "Segundo título!";
+
+// Manupulando o value de um campo
+var campo = document.getElementById("campo");
+campo.value = "Douglas";
+
