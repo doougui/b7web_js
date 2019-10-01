eed1458d858eaf6c1591361b7952d1f5607ed330
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-10-01 15:57:26 -0300

    "Comentários" lesson added

---
+++ Aulas/modulo1_introducao/comentarios/script.js
@@ -0,0 +1,20 @@
+/* 
+Pra que serve
+tantos códigos
+se a vida
+não é programada
+e as melhores coisas
+não tem lógica
+*/
+
+// Definição das variáveis
+var nome = "Douglas"; 
+var idade = 17; // Idade do usuário
+var salvo = true; 
+var cidade; 
+
+// Pegando o tipo das variáveis
+var tipo = typeof nome; 
+
+// Mostrando no console os resultados
+console.log(tipo); 
