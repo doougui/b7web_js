7aedf4e56a2db6a8fe5bccde1d18f251723c3ef8
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-10-01 15:51:04 -0300

    "Tipos de dados" lesson added

---
+++ Aulas/modulo1_introducao/tipos_de_dados/script.js
@@ -0,0 +1,8 @@
+var nome = "Douglas"; // String
+var idade = 17; // Bool
+var salvo = true; // Number
+var cidade; // Variável de tipo indefinido (undefined)
+
+// typeof retorna o tipo da variável 
+var tipo = typeof nome; 
+console.log(tipo); 
