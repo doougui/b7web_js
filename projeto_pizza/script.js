ee6a85caa55c9210012a1db0ef3bef4bf679f5f6
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-10-18 18:54:07 -0300

    "Compra de pizza" updated (3/14).

--- projeto_pizza/script.js
+++ projeto_pizza/script.js
@@ -4,5 +4,10 @@ const cs = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
 	let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+	pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+	pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.', ',')}`;
+	pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+	pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
 	c('.pizza-area').append(pizzaItem);
 });
