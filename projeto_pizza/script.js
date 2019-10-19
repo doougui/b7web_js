e9ae0e16c52b7bfc62006bb440f7031e187705f5
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-10-19 09:17:24 -0300

    "Compra de pizza" updated (8/14)

--- projeto_pizza/script.js
+++ projeto_pizza/script.js
@@ -56,3 +56,23 @@ function closeModal() {
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
 	item.addEventListener('click', closeModal);
 });
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+	if (modalQt > 1) {
+		modalQt--;
+		c('.pizzaInfo--qt').innerHTML = modalQt;
+	}
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+	modalQt++;
+	c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+	size.addEventListener('click', (e) => {
+		c('.pizzaInfo--size.selected').classList.remove('selected');
+		
+		size.classList.add('selected');		
+	});
+});
